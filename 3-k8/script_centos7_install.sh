caa31caaeca4b0615b2f0b378aa72ef65d2f2d93
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2023-07-22 14:26:00 +0200

    Update script_centos7_install.sh
    ajout des parties manquantes du script

--- 3-k8/script_centos7_install.sh
+++ 3-k8/script_centos7_install.sh
@@ -18,4 +18,24 @@ suudo systemctl start docker
 
 sudo yum -y install wget
 
-sudo wget
+sudo wget https://storage.googleapis.com/minikube/releases/v1.11.0/minikube-linux-amd64
+
+sudo chmod +x minikube-linux-amd64
+
+sudo mv minikube-linux-amd64 /usr/bin/minikube
+
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.0/bin/linux/amd64/kubectl
+
+sudo chmod +x kubectl
+
+sudo mv kubectl /usr/bin/
+
+sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+
+sudo systemctl enable docker.service
+
+minikube start --driver=none --kubernetes-version v1.23.0
+
+Pour l'autocomplétion:
+
+echo 'source <(kubectl completion bash)' >> ${HOME}/.bashrc && source ${HOME}/.bashrc
