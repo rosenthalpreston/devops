a996fbd938c12b1bdb28b04ffe6985a1811ecd39
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2023-09-20 22:00:53 +0200

    Update nagios_fr.sh
    update

--- 3- Supervision Nagios/nagios_fr.sh
+++ 3- Supervision Nagios/nagios_fr.sh
@@ -1,50 +1,54 @@
-# Se mettre en root voir créer un mot de passe root pour les nouvelles installations
-su
-apt update && apt install -y sudo
-sudo apt update && sudo apt install -y autoconf gcc libc6 make wget unzip apache2 apache2-utils php libgd-dev openssl libssl-dev iptables-persistent libmcrypt-dev bc gawk dc build-essential snmp libnet-snmp-perl gettext
+#!/bin/bash
+
+# Mise à jour des paquets
+sudo apt-get update
+
+# Installation des prérequis pour Nagios Core
+sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev openssl libssl-dev
+
+# Installation des prérequis pour les plugins Nagios
+sudo apt-get install -y autoconf gcc libc6 libmcrypt-dev make libssl-dev wget bc gawk dc build-essential snmp libnet-snmp-perl gettext
 
 # Téléchargement de la source de Nagios Core
 cd /tmp
 wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.14.tar.gz
 tar xzf nagioscore.tar.gz
-cd nagioscore-nagios-4.4.14/
 
 # Compilation et installation de Nagios Core
-./configure --with-httpd-conf=/etc/apache2/sites-enabled
-make all
-make install-groups-users
-usermod -a -G nagios www-data
-make install
-make install-daemoninit
-make install-commandmode
-make install-config
-make install-webconf
-a2enmod rewrite
-a2enmod cgi
+cd /tmp/nagioscore-nagios-4.4.14/
+sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
 
-# Configuration du pare-feu
-iptables -I INPUT -p tcp --destination-port 80 -j ACCEPT
-iptables-save > /etc/iptables/rules.v4
+# Vérification des demandes éventuelles du Network Manager
+sudo make all
+sudo make install-groups-users
+sudo usermod -a -G nagios www-data
+sudo make install
+sudo make install-daemoninit
+sudo make install-commandmode
+sudo make install-config
+sudo make install-webconf
+sudo a2enmod rewrite
+sudo a2enmod cgi
 
-# Création d'un compte utilisateur pour l'interface web de Nagios
-htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+# Vérification des demandes éventuelles du Network Manager
+sudo ufw allow Apache
+sudo ufw reload
+sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
 
-# Redémarrage des services
-systemctl restart apache2.service
-systemctl start nagios.service
+# Démarrage du serveur web Apache
+sudo systemctl restart apache2.service
 
-# Téléchargement et installation des plugins Nagios
+# Démarrage du service / daemon Nagios Core
+sudo systemctl start nagios.service
+
+# Téléchargement de la source des plugins Nagios
 cd /tmp
 wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.4.6.tar.gz
 tar zxf nagios-plugins.tar.gz
-cd nagios-plugins-release-2.4.6/
+
+# Compilation et installation des plugins Nagios
+cd /tmp/nagios-plugins-release-2.4.6/
 ./tools/setup
 ./configure
 make
-make install
-
-# Commandes pour gérer le service Nagios
-systemctl start nagios.service
-systemctl stop nagios.service
-systemctl restart nagios.service
-systemctl status nagios.service
+sudo make install
