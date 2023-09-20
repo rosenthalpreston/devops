4d312d14530ffa1c7ae18bdaf189c903a3928374
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2023-09-20 16:25:52 +0200

    update

--- 3- Supervision Nagios/nagios_debian_11.sh
+++ 3- Supervision Nagios/nagios_debian_11.sh
@@ -7,9 +7,6 @@ apt install && apt update -y
 
 #Prerequisites
     #Perform these steps to install the pre-requisite packages.
-
-"""===== version debian 9.x / 10.x / 11.x ====="""
-
 apt-get update
 apt-get install -y autoconf gcc libc6 make wget unzip apache2 apache2-utils php libgd-dev
 apt-get install openssl libssl-dev
@@ -40,7 +37,7 @@ make install
 
 
     #cette ligne de commande installe les services et le daemon et les configures pour une éxecution au démarrage.
-Install Service / Daemon
+    # Install Service / Daemon
 make install-daemoninit
  
     #This installs and configures the external command file.
@@ -50,26 +47,19 @@ make install-commandmode
 
     #Install Configuration Files
     #This installs the *SAMPLE* configuration files. These are required as Nagios needs some configuration files to allow it to start.
-
 make install-config
  
-
     #Install Apache Config Files
     #This installs the Apache web server configuration files and configures the Apache settings.
-
 make install-webconf
 a2enmod rewrite
 a2enmod cgi
  
-
     # Configure Firewall
     # You need to allow port 80 inbound traffic on the local firewall so you can reach the Nagios Core web interface.
-
 iptables -I INPUT -p tcp --destination-port 80 -j ACCEPT
 apt-get install -y iptables-persistent
 Answer yes to saving existing rules
-
- 
 
     # Create nagiosadmin User Account
     # You'll need to create an Apache user account to be able to log into Nagios.
@@ -77,10 +67,6 @@ Answer yes to saving existing rules
     # The following command will create a user account called nagiosadmin and you will be prompted to provide a password for the account.
 
 htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
-
-
-    # ===== 8.x / 9.x / 10.x /11.x =====
-
     # Need to restart it because it is already running.
 
 systemctl restart apache2.service  
@@ -88,7 +74,7 @@ Start Service / Daemon
 
  
     # This command starts Nagios Core.
-# ===== 8.x / 9.x / 10.x / 11.x  =====
+
 
 systemctl start nagios.service
 
@@ -113,8 +99,6 @@ make install
 
     # Service / Daemon Commands
     # Different Linux distributions have different methods of starting / stopping / restarting / status Nagios.
-
-
 systemctl start nagios.service
 systemctl stop nagios.service
 systemctl restart nagios.service
